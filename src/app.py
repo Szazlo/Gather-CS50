46506c97e70428aeab4e280de24ebff24610b4e5
Mario <cavalmario2003@gmail.com>
2022-06-21 13:37:11 +0100

    Reorganise database, added register functionality

--- src/app.py
+++ src/app.py
@@ -4,24 +4,26 @@ from urllib import response
 from flask import Flask, render_template, request, make_response, redirect, session, url_for, g
 from flask_session import Session
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import DecimalField
 from forms import *
 from database import get_db, close_db
-from sqlite3 import IntegrityError
+import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
-from helpers import login_required
+#from helpers import login_required
 
 app = Flask(__name__)
+Session(app)
+
+db = sqlite3.connect('app.db')
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SECRET_KEY"] = "Password123"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-app.teardown_appcontext(close_db)
-
-Session(app)
+user_id = []
 
 @app.after_request
 def after_request(response):
@@ -31,24 +33,71 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    form = indexform()
-    registered = False
-    if form.validate_on_submit():
-        email = form.email.data
-        db = get_db()
-        try:
-            session["email"] = email
-            db.execute("""INSERT INTO customers () VALUES ()""", ())
-            db.commit()
-            return redirect("userdetails")
-        except IntegrityError:
-            form.username.errors.append("Username is already taken")
-    return render_template("index.html", form=form, registered=registered)
 
-# @app.route("Home page for logged in user")
-# # Add login requirement when accessing route
-# def dashboard():
-        
-#     return render_template("dashboard.html")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Welcome page"""
+    if request.method == "GET":
+    # Render dashboard if user is logged in
+
+        print("Checking for session id")
+        print(session["username"])
+        try:
+            if session["username"]:
+                return redirect("/dashboard")
+        except:
+            print("No user logged in")
+            return render_template("welcome.html")
+    else:
+        # Redirect to register page.
+        return redirect("/register")
+
+
+            
+@app.route("/dashboard")
+def dashboard():
+    """Dashboard page"""
+    
+    return render_template("dashboard.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register page"""
+    # Define the form 
+    form = registerForm()
+
+    if request.method == "GET":
+        return render_template("register.html", form=form)
+
+    else:
+ 
+        form = registerForm()
+        if form.validate_on_submit():
+            # Get form data
+            firstName = str(form.firstName.data)
+            lastName = str(form.lastName.data)
+            username = db.execute("SELECT username FROM users WHERE username = ?", (form.username.data,)).fetchall()
+
+            if len(username) == 1:
+                return render_template("register.html", form=form, error="Username already exists")
+            
+            username = str(form.username.data)
+
+            email = db.execute("SELECT email FROM users WHERE email = ?", (form.email.data,)).fetchall()
+            if len(email) == 1:
+                return render_template("register.html", form=form, error="Email already exists")
+
+            email = str(form.email.data)
+
+            password = form.password.data
+            # Hash password
+            hashed_password = str(generate_password_hash(password))
+            # Insert user into database
+            db.execute("INSERT INTO users (username, email, password, firstName, lastName) VALUES (?, ?, ?, ?, ?)", 
+                                          (username, email, hashed_password, firstName, lastName))
+            db.commit()
+            print((db.execute("SELECT id FROM users WHERE username = ?", (username,)).fetchall())["username"])
+            session["username"] = (db.execute("SELECT id FROM users WHERE username = ?", (username,)).fetchall())["username"]
+            return redirect("/")
+
