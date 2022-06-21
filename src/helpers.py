896b2ac9481c1d0c23eecb0f72110f4248a9c08e
Mario <cavalmario2003@gmail.com>
2022-06-21 13:39:37 +0100

    Draft login decorator (not used yet)

--- src/helpers.py
+++ src/helpers.py
@@ -1,12 +1,12 @@
 import os
 import requests
+from flask import redirect, render_template, request, session, g
 from functools import wraps
-from flask import redirect, render_template, request, session
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if "email" not in session:
+        if session.get("user_id")is None:
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
